8087489f911dbd12b3792737d17f3bf3ab5e7134
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-08-11 13:31:29 +0200

    runs paginated and searchable

--- src/routes/runs/+page.svelte
+++ src/routes/runs/+page.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
   import { base } from "$app/paths";
-  import { last_annotation } from "$lib/util";
+  import { last_annotation,contains_all_words } from "$lib/util";
   import DatePeriod from "$lib/components/DatePeriod.svelte";
   import RunDisplay from "$lib/components/RunDisplay.svelte";
+  import { paginate, LightPaginationNav } from "svelte-paginate";
   export let data;
 
-  </script>
+  let currentPage = 1;
+  let pageSize = 10;
+  let text_filter = "";
+
+  $: filtered = data.runs.filter((run) => {
+    if (text_filter != "") {
+      return contains_all_words(JSON.stringify(run), text_filter);
+    } else {
+      return true;
+    }
+  });
+  $: paginatedItems = paginate({ items: filtered, pageSize, currentPage });
+</script>
 
 <h1>Runs</h1>
-<!--/* <th>Run ID</th>
-    <th>Finish date</th>
-    <th>Download count</th>
-    <th>Current download link?</th>
-    <th>In working set?</th>
-    <th>Delete date</th>
-    <th>Archive date</th>
-    <th>Archive end date</th>
-    <th>Archive size (GB)</th>
-    <th>In archive</th> */
-	-->
-{#each Object.values(data.runs) as run}
+Filter:<input type="text" bind:value={text_filter} />
+  {#each paginatedItems as run}
   <table>
     <tr><td colspan="2"> <RunDisplay data={run} /></td></tr>
     <tr>
@@ -31,12 +34,12 @@
       <td>{run.download_count}</td>
     </tr>
 
-	{#if last_annotation(run, "receivers") }
-	<tr>
-		<th>Receiver</th>
-		<td>{last_annotation(run, "receivers")}</td>
-	</tr>
-	{/if}
+    {#if last_annotation(run, "receivers")}
+      <tr>
+        <th>Receiver</th>
+        <td>{last_annotation(run, "receivers")}</td>
+      </tr>
+    {/if}
     <tr>
       <th>In working set</th>
       <td
@@ -64,36 +67,38 @@
           />
         </td>
       </tr>
-	  <tr>
+      <tr>
         <th>Actual archival remove date</th>
         <td>
-          <DatePeriod
-            timestamp={run.archive_deletion_date}
-          />
+          <DatePeriod timestamp={run.archive_deletion_date} />
         </td>
       </tr>
-	  {#if run.estimated_archive_size != null}
-		  <tr>
-			  <th>Future Size in archive (in GB)</th>
-			  <td>{Math.round(run.estimated_archive_size / 1024. / 1024. / 1024. * 100) / 100}</td>
-		  </tr>
-	  {/if}
-	  {#if run.archive_date != null && run.archive_deletion_date == null}
-		  <tr>
-			<th>Current Size in archive (in GB)</th>
-			<td
-			  >{#if run.archive_size}
-				{Math.round(run.archive_size / 1024. / 1024. / 1024. * 100) / 100}
-			  {/if}
-			</td>
-		  </tr>
-	  {/if}
-	<tr>
-		<th>Archive removal warning mail sent</th>
-		<td>{#if run.archive_deletion_warning_sent}yes{:else}no{/if}</td>
-	</tr>
-
-
+      {#if run.estimated_archive_size != null}
+        <tr>
+          <th>Future Size in archive (in GB)</th>
+          <td
+            >{Math.round(
+              (run.estimated_archive_size / 1024 / 1024 / 1024) * 100
+            ) / 100}</td
+          >
+        </tr>
+      {/if}
+      {#if run.archive_date != null && run.archive_deletion_date == null}
+        <tr>
+          <th>Current Size in archive (in GB)</th>
+          <td
+            >{#if run.archive_size}
+              {Math.round((run.archive_size / 1024 / 1024 / 1024) * 100) / 100}
+            {/if}
+          </td>
+        </tr>
+      {/if}
+      <tr>
+        <th>Archive removal warning mail sent</th>
+        <td
+          >{#if run.archive_deletion_warning_sent}yes{:else}no{/if}</td
+        >
+      </tr>
     {:else}
       <tr>
         <th>Archive</th>
@@ -108,14 +113,26 @@
         <td> <DatePeriod timestamp={run.delete_date} /> </td>
       </tr>
     {/if}
-	<tr>
-		<th>Deletion/Archive warning mail sent</th>
-		<td>{#if run.deletion_warning_sent}yes{:else}no{/if}</td>
-	</tr>
-	  </table>
+    <tr>
+      <th>Deletion/Archive warning mail sent</th>
+      <td
+        >{#if run.deletion_warning_sent}yes{:else}no{/if}</td
+      >
+    </tr>
+  </table>
   <br />
   <br />
 {/each}
+<LightPaginationNav
+  totalItems={filtered.length}
+  {pageSize}
+  {currentPage}
+  limit={1}
+  showStepOptions={true}
+  on:setPage={(e) => (currentPage = e.detail.page)}
+/>
+
+
 
 <style>
   th,
