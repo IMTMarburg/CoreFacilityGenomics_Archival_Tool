8ca4d63ecb9b964eb2b0b57c0db52d882ab5884c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-10-10 12:00:27 +0200

    fix gzip compression

--- python/archival_tool.py
+++ python/archival_tool.py
@@ -185,7 +185,7 @@ def tar_and_encrypt(input_folder, output_file):
         # "--exclude",
         # "Data",
         "--use-compress-program",
-        "gz",
+        "gzip",
         # "zstd -19",
         "-c",
         str(input_folder.name),
